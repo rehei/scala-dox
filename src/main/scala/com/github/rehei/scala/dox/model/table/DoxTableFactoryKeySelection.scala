19230460993933c81518c2c6cd33a06c39b2689d
Heiner Reinhardt <heiner.reinhardt@googlemail.com>
2021-06-05 13:58:54 +0200

    some adjustment

--- src/main/scala/com/github/rehei/scala/dox/model/table/DoxTableFactoryKeySelection.scala
+++ src/main/scala/com/github/rehei/scala/dox/model/table/DoxTableFactoryKeySelection.scala
@@ -5,7 +5,7 @@ import com.github.rehei.scala.macros.Query
 case class DoxTableFactoryKeySelection[T](query: Query[T]) {
   def take(property: Query[T] => Query[_]) = new {
     def config(config: DoxTableKeyConfig) = {
-      new DoxTableFactoryKey(property(query), config)
+      DoxTableFactoryKey(property(query), config)
     }
   }
 }
