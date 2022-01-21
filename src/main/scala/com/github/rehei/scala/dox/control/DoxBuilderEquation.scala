e049eb424fc79f461501420ccac3ba3b2963dd6a
Heiner Reinhardt <heiner.reinhardt@googlemail.com>
2022-01-21 14:11:09 +0100

    Some change

--- src/main/scala/com/github/rehei/scala/dox/control/DoxBuilderEquation.scala
+++ src/main/scala/com/github/rehei/scala/dox/control/DoxBuilderEquation.scala
@@ -4,11 +4,16 @@ import com.github.rehei.scala.dox.model.DoxTableViewModel
 import com.github.rehei.scala.dox.model.reference.DoxReferencePersistentEquation
 import com.github.rehei.scala.dox.model.table.DoxTable
 import com.github.rehei.scala.dox.model.DoxEquation
+import com.github.rehei.scala.dox.text.TextAST
+import com.github.rehei.scala.dox.text.util.Text2TEX
 
 object DoxBuilderEquation {
   def label(_labelOption: Option[DoxReferencePersistentEquation]) = new {
     def data(_equation: String) = {
       DoxEquation(_equation, _labelOption)
+    }
+    def data(_equation: TextAST) = {
+      DoxEquation(Text2TEX.apply(true).generate(_equation), _labelOption)
     }
   }
 }
