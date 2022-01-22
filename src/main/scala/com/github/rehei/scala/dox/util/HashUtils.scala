e6a6e26fdbca7cc621b30ab3726c1dfa672abf61
Heiner Reinhardt <heiner.reinhardt@googlemail.com>
2022-01-22 12:43:19 +0100

    Adjusting hash utils

--- src/main/scala/com/github/rehei/scala/dox/util/HashUtils.scala
+++ src/main/scala/com/github/rehei/scala/dox/util/HashUtils.scala
@@ -5,13 +5,33 @@ import org.apache.commons.codec.binary.Base32
 import java.math.BigInteger
 import org.apache.commons.codec.digest.DigestUtils
 import java.security.MessageDigest
-
+import scala.collection.JavaConversions._
+import org.apache.commons.codec.digest.MessageDigestAlgorithms
 
 object HashUtils {
-    def hash(input: String) = {
-    val md2Base16 = new DigestUtils(org.apache.commons.codec.digest.MessageDigestAlgorithms.SHA_1).digestAsHex(input)
+
+  import MessageDigestAlgorithms._
+  
+  protected val RADIX = 32
+
+  def hash(input: String) = {
+    val md2Base16 = new DigestUtils(SHA_1).digestAsHex(input)
     val base10 = new BigInteger(md2Base16, 16)
-    val base64 = new String(Base64.encodeInteger(base10))
-    base64.substring(0, 5).replace('/', '-')
+    val base22 = base10.toString(22)
+
+    base22.toCharArray().take(5).map(convert(_)).mkString
   }
+
+  protected def convert(char: Char) = {
+    val value = {
+      if (Character.isDigit(char)) {
+        char.toString().toInt
+      } else {
+        Character.digit(char, RADIX)
+      }
+    }
+    Character.forDigit(value + 10, RADIX)
+  }
+
+
 }
