e712211ea69440d45ecc4662db4e345fafa651b3
Heiner Reinhardt <heiner.reinhardt@googlemail.com>
2022-01-30 22:19:03 +0100

    Some cleanup

--- src/main/scala/com/github/rehei/scala/dox/model/table/DoxTableAlignment.scala
+++ src/main/scala/com/github/rehei/scala/dox/model/table/DoxTableAlignment.scala
@@ -2,7 +2,6 @@ package com.github.rehei.scala.dox.model.table
 
 object DoxTableAlignment {
 
-  val NONE = DoxTableAlignment("")
   val LEFT = DoxTableAlignment("LEFT")
   val RIGHT = DoxTableAlignment("RIGHT")
   val CENTER = DoxTableAlignment("CENTER")
