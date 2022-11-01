89e01f0a95feb9962e9f3de563ce3a29cfe0f142
Heiner Reinhardt <heiner.reinhardt@googlemail.com>
2022-11-01 10:49:09 +0100

    Fixing some size calculation

--- src/main/scala/com/github/rehei/scala/dox/control/tex/ColumnType.scala
+++ src/main/scala/com/github/rehei/scala/dox/control/tex/ColumnType.scala
@@ -3,27 +3,31 @@ package com.github.rehei.scala.dox.control.tex
 import java.text.DecimalFormat
 
 object ColumnType {
-  
-  def l(size: Option[Double]) = slashed("""\raggedright""", size)
-  def c(size: Option[Double]) = slashed("""\centering""", size)
-  def r(size: Option[Double]) = slashed("""\raggedleft""", size)
-  
-  def lMinipage(text:String) = s"""\\begin{flushleft}${text}\\end{flushleft}"""
-  def cMinipage(text:String) = s"""\\begin{center}${text}\\end{center}"""
-  def rMinipage(text:String) = s"""\\begin{flushright}${text}\\end{flushright}"""
-  
+
+  def l() = "l"
+  def c() = "c"
+  def r() = "r"
+
+  def l(size: Double) = slashed("""\raggedright""", size)
+  def c(size: Double) = slashed("""\centering""", size)
+  def r(size: Double) = slashed("""\raggedleft""", size)
+
+  def lMinipage(text: String) = s"""\\begin{flushleft}${text}\\end{flushleft}"""
+  def cMinipage(text: String) = s"""\\begin{center}${text}\\end{center}"""
+  def rMinipage(text: String) = s"""\\begin{flushright}${text}\\end{flushright}"""
+
   def numeric(size: Double) = "S[table-number-alignment=center, table-column-width=" + size + "cm]"
-  
-  protected def slashed(command: String, size: Option[Double]) = {
-    ">{" + command + """\arraybackslash""" + "}p" + size.map(sizeString(_)).getOrElse("")
+
+  protected def slashed(command: String, size: Double) = {
+    ">{" + command + """\arraybackslash""" + "}" + sizeString(size)
   }
-  
+
   protected def sizeString(size: Double) = {
     val df = new DecimalFormat("#")
     df.setMinimumIntegerDigits(1)
     df.setMaximumFractionDigits(3)
     val stringValue = df.format(size)
 
-    "{" + stringValue + "cm}"
+    "p{" + stringValue + "cm}"
   }
 }
